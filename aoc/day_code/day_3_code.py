356feb1ae186a6e9dc2beb71928cce4bf8cd02fa
mhoskin-dsc <michael.hoskin@ons.gov.uk>
2021-12-03 20:10:42 +0000

    day 3 inputs and part 1 code'

--- aoc/day_code/day_3_code.py
+++ aoc/day_code/day_3_code.py
@@ -5,12 +5,35 @@ from aoc.utilities import time_wrap
 _DAY = 3
 
 
+def part_1(data):
+    
+    bit_length = len(data[0])
+    diag_length = len(data)
+    
+    sums = [0] * bit_length
+
+    for diag in data:
+        
+        for bit in range(bit_length):
+            sums[bit] += int(diag[bit])
+
+    gamma = [0] * bit_length
+    epsilon = gamma.copy()
+    
+    for bit in range(bit_length):
+        gamma[bit] = str(int(sums[bit] > diag_length/2))
+        epsilon[bit] = str(1 - int(gamma[bit]))
+
+    pwr = int(''.join(gamma), 2) * int(''.join(epsilon), 2)
+    
+    return pwr
+
 @time_wrap
 def day():
     
-    data = read_input(_DAY,test=True)
+    data = read_input(_DAY, split='', test=False)
     
-    p1 = 1
+    p1 = part_1(data)
     p2 = 2
     
     print("Day {0} Part 1: {1}".format(_DAY, p1))
