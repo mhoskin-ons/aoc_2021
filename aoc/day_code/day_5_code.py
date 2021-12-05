994871f62a904e551b67a4add944fcb629b3ec22
mhoskin-dsc <michael.hoskin@ons.gov.uk>
2021-12-05 09:28:41 +0000

    added data for d5, initial data prep func

--- aoc/day_code/day_5_code.py
+++ aoc/day_code/day_5_code.py
@@ -1,9 +1,20 @@
 from aoc.read import read_input
 from aoc.utilities import time_wrap
 
+from dataclasses import dataclass 
 
 _DAY = 5
    
+
+@dataclass
+class Coord:
+    x: int
+    y: int
+    
+    # def __lt__(self, Coord):
+        
+    #     return ((self.x < Coord.x) & ()
+
 
 def part_1(data):
     
@@ -14,6 +25,20 @@ def part_2(data):
 
     return data
 
+
+def get_data(test=True):
+    
+    data = read_input(_DAY, test=True)
+    
+    all_points = []
+    for d in data:
+        points = d.split(' -> ')
+        init_point = [int(p) for p in points[0].split(',')]
+        final_point = [int(p) for p in points[1].split(',')]
+
+        all_points.append((init_point, final_point))
+
+    return all_points
 
 @time_wrap
 def day():
--- aoc/days.py
+++ aoc/days.py
@@ -31,8 +31,129 @@ import day_code.day_4_code as d4
 
 d4p1, d4p2 = d4.day()
 
-# %% Day 3
+# %% Day 5
 
 # import day_code.day_5_code as d5
 
 # d5p1, d5p2 = d5.day()
+
+# %% Day 6
+
+# import day_code.day_6_code as d6
+
+# d6p1, d6p2 = d6.day()
+
+# %% Day 7
+
+# import day_code.day_7_code as d7
+
+# d7p1, d7p2 = d7.day()
+
+# %% Day 8
+
+# import day_code.day_8_code as d8
+
+# d8p1, d8p2 = d8.day()
+
+# %% Day 9
+
+# import day_code.day_9_code as d9
+
+# d9p1, d9p2 = d9.day()
+
+# %% Day 10
+
+# import day_code.day_10_code as d10
+
+# d10p1, d10p2 = d10.day()
+
+# %% Day 11
+
+# import day_code.day_11_code as d11
+
+# d11p1, d11p2 = d11.day()
+
+# %% Day 12
+
+# import day_code.day_12_code as d12
+
+# d12p1, d12p2 = d12.day()
+
+# %% Day 13
+
+# import day_code.day_13_code as d13
+
+# d13p1, d13p2 = d13.day()
+
+# %% Day 14
+
+# import day_code.day_14_code as d14
+
+# d14p1, d14p2 = d14.day()
+
+# %% Day 15
+
+# import day_code.day_15_code as d15
+
+# d15p1, d15p2 = d15.day()
+
+# %% Day 16
+
+# import day_code.day_16_code as d16
+
+# d16p1, d16p2 = d16.day()
+
+# %% Day 17
+
+# import day_code.day_17_code as d17
+
+# d17p1, d17p2 = d17.day()
+
+# %% Day 18
+
+# import day_code.day_18_code as d18
+
+# d18p1, d18p2 = d18.day()
+
+# %% Day 19
+
+# import day_code.day_19_code as d19
+
+# d19p1, d19p2 = d19.day()
+
+# %% Day 20
+
+# import day_code.day_20_code as d20
+
+# d20p1, d20p2 = d20.day()
+
+# %% Day 21
+
+# import day_code.day_21_code as d21
+
+# d21p1, d21p2 = d21.day()
+
+# %% Day 22
+
+# import day_code.day_22_code as d22
+
+# d22p1, d22p2 = d22.day()
+
+# %% Day 23
+
+# import day_code.day_23_code as d23
+
+# d23p1, d23p2 = d23.day()
+
+# %% Day 24
+
+# import day_code.day_24_code as d24
+
+# d24p1, d24p2 = d24.day()
+
+# %% Day 25
+
+# import day_code.day_25_code as d25
+
+# d25p1, d25p2 = d25.day()
+
